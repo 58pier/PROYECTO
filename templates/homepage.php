ae0a6c0bf86f44b5d1f5d81590eb888f23d0b6dc
Piero <pieropozo58@gmail.com>
2020-07-04 23:49:01 -0500

    homepage

---
+++ templates/homepage.php
@@ -0,0 +1,63 @@
+<?php include "include/header.php" ?>
+
+<div class="slider">
+
+</div>
+
+<div class="container-categoria">
+    <h3> ACCION </h3>
+    <div class="container-juego">
+        <table> 
+            <tr> 
+                  <?php  /*FOR EACH -> articulo hasta 4 */ ?>
+
+                <td> 
+                <div class="juego-homepage">
+                    <img src="">
+                </div>
+                </td>
+                <?php /*Termina el for */?>
+            </tr>
+        </table>
+        <h3><a>VER MAS</a></h3>
+    </div>
+</div>
+<div class="container-categoria">
+    <h3> AVENTURA </h3>
+    <div class="container-juego">
+        <table> 
+            <tr> 
+                  <?php  /*FOR EACH -> articulo hasta 4 */ ?>
+
+                <td> 
+                <div class="juego-homepage">
+                    <img src="">
+                </div>
+                </td>
+                <?php /*Termina el for */?>
+            </tr>
+        </table>
+        <h3><a>VER MAS</a></h3>
+    </div>
+</div>
+<div class="container-categoria">
+    <h3> HORROR </h3>
+    <div class="container-juego">
+        <table> 
+            <tr> 
+                  <?php  /*FOR EACH -> articulo hasta 4 */ ?>
+
+                <td> 
+                <div class="juego-homepage">
+                    <img src="">
+                </div>
+                </td>
+                <?php /*Termina el for */?>
+            </tr>
+        </table>
+        <h3><a>VER MAS</a></h3>
+    </div>
+</div>
+
+
+<?php include "include/footer.php"?>
