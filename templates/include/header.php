b6326d0ae4fcce2a0abdf1fe5942fcf9c6c90a9c
Piero <pieropozo58@gmail.com>
2020-07-04 22:35:50 -0500

    Estilos

--- templates/include/header.php
+++ templates/include/header.php
@@ -8,20 +8,22 @@
 </head>
 <body>
 
-    <div class="menu">
-        <div class="logo">
-            <img src="../../img/logo.png" >
-        </div>
-        <div class="categorias">
-            <nav>
-                <ul>
-                    <li><a>ACCION</a></li>
-                    <li><a>AVENTURA</a></li>
-                    <li><a>HORROR</a></li>
-                    <li><a >INDIE</a></li>
-                </ul>
-            </nav>
-        </div>
+    <div class="container-menu">
+        <div class="menu">
+            <div class="logo">
+                <img src="../../img/logo.png" >
+            </div>
+            <div class="categorias">
+                <nav>
+                    <ul>
+                        <li><a>ACCION</a></li>
+                        <li><a>AVENTURA</a></li>
+                        <li><a>HORROR</a></li>
+                        <li><a >INDIE</a></li>
+                    </ul>
+                </nav>
+            </div>
+    </div>
     </div>
 
 
