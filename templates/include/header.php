c69f967537faf785f6d5822d4df28b05e263dbfc
Piero <pieropozo58@gmail.com>
2020-07-04 21:47:24 -0500

    HEADER

---
+++ templates/include/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/estilos.css">
+    <title>Document</title>
+</head>
+<body>
+
+    <div class="menu">
+        <div class="logo">
+            <img src="/aadsasd" >
+        </div>
+        <div class="categorias">
+            <nav>
+                <ul>
+                    <li><a>ACCION</a></li>
+                    <li><a>AVENTURA</a></li>
+                    <li><a>HORROR</a></li>
+                    <li><a >INDIE</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+
+
+
+
+
