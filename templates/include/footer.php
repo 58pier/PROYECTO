1a90d29f7466d9e8e6d1023b3c7008f481b06247
JmAo010902 <67839157+JmAo010902@users.noreply.github.com>
2020-07-04 22:35:40 -0500

    Footer

---
+++ templates/include/footer.php
@@ -0,0 +1,9 @@
+<?php include "header.php" ?>
+        
+    </body>
+    <footer>
+        <div class="footer">
+	        <p>©Copyrigth JMP, Todos los derechos reservados</p>
+        </div>
+    </footer>
+</html>
