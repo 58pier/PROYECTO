c6610d531a0da5284808fb6574decb6ee3a22b15
Piero <pieropozo58@gmail.com>
2020-07-15 21:24:10 -0500

    Categoria

---
+++ templates/categoria.php
@@ -0,0 +1,36 @@
+<?php include "include/header.php" ?>
+
+
+
+<div class="container-body">
+    <h1> Accion<?php //Categoria 
+                ?> </h1>
+    <div class="portada-categoria">
+        <img src="">
+    </div>
+    <div class="container-categoria">
+
+        <table>
+            <?php //for para i<3 
+            ?>
+            <tr>
+                <?php  /*FOR EACH -> articulo hasta 4 */ ?>
+
+                <td>
+                    <div class="juego-homepage">
+                        <img src="">
+                        <?php //titulo 
+                        ?>
+                    </div>
+                </td>
+                <?php /*Termina el for */ ?>
+            </tr>
+            <?php //termina el for de i 
+            ?>
+        </table>
+    </div>
+</div>
+
+
+
+<?php include "include/footer.php" ?>
