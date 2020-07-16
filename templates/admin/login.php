ef95fd72378b535fbe0c6214aea0cbe1fecf47e2
Piero <pieropozo58@gmail.com>
2020-07-16 14:30:44 -0500

    Create login.php

---
+++ templates/admin/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/estilos.css">
+    <title>Document</title>
+</head>
+
+<body class="degradado">
+    <div class="login"><br>
+        <img src="../../img/logo.png">
+        <h1>Login</h1>
+        <input type="text" value="Usser" id="usser" class="usser"><br><br><br>
+        <input type="password" value="Pass" id="pass" class="usser"><br><br><br>
+        <input type="button" value="Iniciar Sesión" id="ingresar" class="button">
+    </div>
+</body>
+
+</html>
