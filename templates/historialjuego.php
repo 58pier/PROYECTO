9c1d62880111c90ab0a344c04b4ad74cb47455f2
JmAo010902 <67839157+JmAo010902@users.noreply.github.com>
2020-07-23 19:19:04 -0500

    miralo

---
+++ templates/historialjuego.php
@@ -0,0 +1,47 @@
+<?php include "include/header.php" ?>
+<div class="title_game">
+    <h1>Titulo</h1>
+    <div class="game_img">
+        <img src="" ><?php  /*CARRUSSEL DE IMAGENES DEL JUEGO*/ ?>
+    </div>
+</div>
+<div>
+    <div class="logo_juego">
+        <img src="" > <?php  /*LOGO DEL JUEGO */ ?>
+        <div class="resumen">
+            
+        <h1>Resumen</h1> <?php  /*Resumen*/ ?><h3>
+            </h3>
+            <div class="compra">
+                <input type="button" value="COMPRAR">
+            </div>
+        </div>
+    </div>    
+</div>
+<div class="acerca">
+    <h2>Acerca del juego:</h2>
+    <table class="tabla1"><thead> <tr> <th colspan='1'>DESARROLLADOR</th><th colspan='1'>EDITORA</th><th colspan='1'>FECHA DE LANZAMIENTO</th><th colspan='1'>ETIQUETA</th><th colspan='1'>CLASIFICACION</th><th colspan='1'>PLATAFORMA</th></tr></thead>
+    </table>
+</div>
+<div class="language">
+    <h2>Idiomas:</h2>
+</div>
+<div class="descripcion">
+    <h2>Descripcion:&nbsp&nbsp&nbsp&nbsp</h2>
+    <div class="descr_text">
+        <h3></h3><?php  /*Descripcion*/ ?>
+    </div>
+</div>
+<div class="acerca">
+    <h2>Requerimientos:</h2>
+    <table class="tabla1"><thead> <tr> <th colspan='1'>PROCESADOR</th><th colspan='1'>SO</th><th colspan='1'>MEMORIA RAM</th><th colspan='1'>TARJETA GRÁFICA</th></tr></thead>
+    </table>
+</div>
+<div class="descripcion">
+    <h2>Más informacion: <a href="">Click aqui...</a></h2><?php/*Link del juego */ ?>
+</div>
+<div class="derechos_de_autor">
+    <h2>Derechos de autor:</h2><h3></h3><?php/*Derechos de autor*/ ?>
+</div>
+
+<?php include "include/footer.php"?>
