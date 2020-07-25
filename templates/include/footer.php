344564e3bf5017f02b74eb47e4b4f5f70fba99cd
Piero <pieropozo58@gmail.com>
2020-07-25 12:09:42 -0500

    Slick

--- templates/include/footer.php
+++ templates/include/footer.php
@@ -1,8 +1,12 @@
-      </div> 
-    </body>
+    </div>
     <footer>
         <div class="footer">
-	        <p>©Copyrigth JMP, Todos los derechos reservados</p>
+            <p>©Copyrigth JMP, Todos los derechos reservados</p>
         </div>
     </footer>
-</html>
+
+    <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <script type="text/javascript" src="slick/slick/slick.min.js"></script>
+    </body>
+    </html>
--- templates/include/header.php
+++ templates/include/header.php
@@ -5,6 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/estilos.css">
+    <link rel="stylesheet" type="text/css" href="slick/slick/slick.css" />
+    <link rel="stylesheet" type="text/css" href="slick/slick//slick-theme.css" />
+
     <title><?php echo htmlspecialchars($results['pageTitle']) ?></title>
 </head>
 
