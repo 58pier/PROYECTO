33e343ea10e67c2f3b728f51891a77cd6aed10c4
Piero <pieropozo58@gmail.com>
2020-07-27 09:57:27 -0500

    Update editJuego.php

--- templates/admin/editJuego.php
+++ templates/admin/editJuego.php
@@ -16,37 +16,37 @@
     <input type="text" name="nombre" id="nombre" placeholder="Nombre del juego" required autofocus maxlength="255" value="<?php echo htmlspecialchars($results['juego']->nombre) ?>">
 
     <label for="idiomas"> Idiomas del Juego</label>
-    <input type="text" name="idiomas" id="idiomas" placeholder="Idiomas del juego"  maxlength="255" value="<?php echo htmlspecialchars($results['juego']->idiomas) ?>">
+    <input type="text" name="idiomas" id="idiomas" placeholder="Idiomas del juego" maxlength="255" value="<?php echo htmlspecialchars($results['juego']->idiomas) ?>">
 
     <label for="precio"> Precio del Juego</label>
-    <input type="number" name="precio" id="precio" placeholder="Precio del juego"  value="<?php echo htmlspecialchars($results['juego']->precio) ?>">
+    <input type="number" name="precio" id="precio" placeholder="Precio del juego" value="<?php echo htmlspecialchars($results['juego']->precio) ?>">
 
     <label for="desarrollador"> Desarrolador del Juego</label>
-    <input type="text" name="desarrollador" id="desarrollador" placeholder="Desarrollador del juego"  maxlength="255" value="<?php echo htmlspecialchars($results['juego']->desarrollador) ?>">
+    <input type="text" name="desarrollador" id="desarrollador" placeholder="Desarrollador del juego" maxlength="255" value="<?php echo htmlspecialchars($results['juego']->desarrollador) ?>">
 
     <label for="plataforma"> Plataforma del Juego</label>
-    <input type="text" name="plataforma" id="plataforma" placeholder="Plataforma del juego"  maxlength="255" value="<?php echo htmlspecialchars($results['juego']->plataforma) ?>">
+    <input type="text" name="plataforma" id="plataforma" placeholder="Plataforma del juego" maxlength="255" value="<?php echo htmlspecialchars($results['juego']->plataforma) ?>">
 
     <label for="clasificacion"> Clasificación del Juego</label>
-    <input type="text" name="clasificacion" id="clasificacion" placeholder="Clasificacion del juego"  maxlength="255" value="<?php echo htmlspecialchars($results['juego']->clasificacion) ?>">
+    <input type="text" name="clasificacion" id="clasificacion" placeholder="Clasificacion del juego" maxlength="255" value="<?php echo htmlspecialchars($results['juego']->clasificacion) ?>">
 
     <label for="resumen"> Resumen del Juego</label>
-    <textarea name="resumen" id="resumen" placeholder="Resumen del juego"  maxlength="100000" value="<?php echo htmlspecialchars($results['juego']->resumen) ?>"> </textarea>
+    <textarea name="resumen" id="resumen" placeholder="Resumen del juego" required maxlength="100000"><?php echo htmlspecialchars($results['juego']->resumen) ?></textarea>
 
     <label for="requerimientos"> Requerimientos del Juego</label>
-    <input type="text" name="requerimientos" id="requerimientos" placeholder="Requerimientos del juego"  maxlength="255" value="<?php echo htmlspecialchars($results['juego']->requerimientos) ?>">
+    <input type="text" name="requerimientos" id="requerimientos" placeholder="Requerimientos del juego" maxlength="255" value="<?php echo htmlspecialchars($results['juego']->requerimientos) ?>">
 
     <label for="pagina_web"> Pagina Web del Juego</label>
-    <input type="text" name="pagina_web" id="pagina_web" placeholder="Pagina Web del juego"  maxlength="255" value="<?php echo htmlspecialchars($results['juego']->pagina_web) ?>">
+    <input type="text" name="pagina_web" id="pagina_web" placeholder="Pagina Web del juego" maxlength="255" value="<?php echo htmlspecialchars($results['juego']->pagina_web) ?>">
 
     <label for="derechos_autor"> Derechos del autor del Juego</label>
-    <input type="text" name="derechos_autor" id="derechos_autor" placeholder="Derechos de autor del juego"  maxlength="255" value="<?php echo htmlspecialchars($results['juego']->derechos_autor) ?>">
+    <input type="text" name="derechos_autor" id="derechos_autor" placeholder="Derechos de autor del juego" maxlength="255" value="<?php echo htmlspecialchars($results['juego']->derechos_autor) ?>">
 
     <label for="trailer"> Trailer del Juego</label>
-    <input type="text" name="trailer" id="trailer" placeholder="Trailer del juego"  maxlength="255" value="<?php echo htmlspecialchars($results['juego']->trailer) ?>">
+    <input type="text" name="trailer" id="trailer" placeholder="Trailer del juego" maxlength="255" value="<?php echo htmlspecialchars($results['juego']->trailer) ?>">
 
     <label for="descripcion"> Descripcion del Juego</label>
-    <textarea name="descripcion" id="descripcion" placeholder="Descripcion del juego"  maxlength="100000" value="<?php echo htmlspecialchars($results['juego']->descripcion) ?>"> </textarea>
+    <textarea name="descripcion" id="descripcion" placeholder="Descripcion del juego" required maxlength="100000"><?php echo htmlspecialchars($results['juego']->descripcion) ?></textarea>
 
     <label for="categoriaId"> Categoria del Juego</label>
     <select name="categoriaId">
@@ -57,7 +57,7 @@
     </select>
 
     <label for="fecha"> Fecha del Juego</label>
-    <input type="date" name="fecha" id="fecha" placeholder="YYYY-MM-DD"  maxlength="10" value="<?php echo htmlspecialchars($results['juego']->fecha) ? date("Y-m-d", $results['juego']->fecha) : "" ?>">
+    <input type="date" name="fecha" id="fecha" placeholder="YYYY-MM-DD" maxlength="10" value="<?php echo htmlspecialchars($results['juego']->fecha) ? date("Y-m-d", $results['juego']->fecha) : "" ?>">
 
 
     <div class="buttons">
