5e7f24fa34f4abd6b50026089211c4702d48f2f3
Piero <pieropozo58@gmail.com>
2020-08-01 13:23:39 -0500

    Categoria front end

--- templates/archive.php
+++ templates/archive.php
@@ -3,33 +3,45 @@
 
 
 <div class="container-body">
-    <h1> <?php echo htmlspecialchars( $results['pageHeading'])?> </h1>
-    <div class="portada-categoria">
-        <img src="">
-    </div>
-    <div class="container-categoria">
-
-        <table>
-            <?php //for para i<3 
-            ?>
-            <tr>
-                <?php  /*FOR EACH -> articulo hasta 4 */ ?>
-
-                <td>
-                    <div class="juego-homepage">
-                        <img src="">
-                        <?php //titulo 
-                        ?>
+    <h1> <?php echo htmlspecialchars($results['pageHeading']) ?> </h1>
+    <div class="slider">
+        <?php foreach ($results['juegos'] as $juego) { ?>
+            <div class="slide">
+                <a href=".?action=viewJuego&amp;juegoId=<?php echo $juego->id ?>">
+                    <div class="slide-img">
+                        <img src="<?php echo JUEGO_IMAGE_PATH . "/" . htmlspecialchars($juego->getImages(0)) ?>">
                     </div>
-                </td>
-                <?php /*Termina el for */ ?>
-            </tr>
-            <?php //termina el for de i 
-            ?>
-        </table>
+                    <div class="slide-title">
+                        <span> <?php echo ($juego->nombre) ?></span>
+                    </div>
+            </div>
+            </a>
+        <?php } ?>
     </div>
 </div>
-
-
+<div>
+    <div>
+        <?php foreach ($results['juegos'] as $juego) { ?>
+                <div class="slide-juego">
+                    <a href=".?action=viewJuego&amp;juegoId=<?php echo $juego->id ?>">
+                        <div class="slide-img">
+                            <img src="<?php echo JUEGO_IMAGE_PATH . "/" . htmlspecialchars($juego->getImages(0)) ?>">
+                        </div>
+                        <div class="slide-title">
+                            <span> <?php echo ($juego->nombre) ?></span>
+                        </div>
+                </div>
+        <?php } ?>
+    </div>
+</div>
+<?php include "include/final-divs.php" ?>
+<script type="text/javascript">
+    $(document).ready(function() {
+        $(".slider").slick({
+            lazyLoad: 'ondemand', // ondemand progressive anticipated
+            infinite: true
+        });
+    });
+</script>
 
 <?php include "include/footer.php" ?>
