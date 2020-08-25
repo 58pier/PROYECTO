8c0d0f0c067e0b41f40d041b8ad3d6c21060617a
Piero <pieropozo58@gmail.com>
2020-08-25 14:08:34 -0500

    Buscador
    Se implemento un buscador de juegos al homepage

--- templates/homepage.php
+++ templates/homepage.php
@@ -1,95 +1,94 @@
 <?php include "include/header.php" ?>
-
-<div class="slider">
-    <?php foreach ($results['juegos'] as $juego) { ?>
-        <div class="slide">
-            <a href="juego_<?php echo $juego->id ?>">
-                <div class="slide-title">
-                    <span> <?php echo ($juego->nombre) ?></span>
-                </div>
-                <div class="slide-img">
-                    <img src="<?php echo JUEGO_IMAGE_PATH . "/" . htmlspecialchars($juego->getImages(0)) ?>">
-                </div>
-            </a>
-        </div>
-
-    <?php } ?>
-</div>
-
-<div class="body-homepage">
-    <?php foreach ($results['categorias'] as $categoria) { ?>
-        <div class="container-categoria">
-            <h2 class="category"> <a href="categoria_<?php echo $categoria->id ?>"><?php echo htmlspecialchars($categoria->nombre) ?></a> </h2>
-            <div class="container-juego">
-                <?php foreach ($results['juegos'] as $juego) { ?>
-                    <?php if ($juego->categoriaId == $categoria->id) { ?>
-                        <div class="slide-juego">
-
-                            <div class="slide-img2">
-                                <a href="juego_<?php echo $juego->id ?>"><img src="<?php echo JUEGO_IMAGE_PATH . "/" . htmlspecialchars($juego->getImages(0)) ?>"></a>
-                            </div>
-                            <div class="slide-title-juego">
-                                <a href="juego_<?php echo $juego->id ?>"><span> <?php echo ($juego->nombre) ?></span></a>
-                            </div>
-
-                        </div>
-                    <?php } ?>
-                <?php } ?>
+    <div class="slider">
+        <?php foreach ($results['juegos'] as $juego) { ?>
+            <div class="slide">
+                <a href="juego_<?php echo $juego->id ?>">
+                    <div class="slide-title">
+                        <span> <?php echo ($juego->nombre) ?></span>
+                    </div>
+                    <div class="slide-img">
+                        <img src="<?php echo JUEGO_IMAGE_PATH . "/" . htmlspecialchars($juego->getImages(0)) ?>">
+                    </div>
+                </a>
             </div>
-            <h3><a href="categoria_<?php echo $categoria->id ?>">VER MAS</a></h3>
-        </div>
-    <?php } ?>
 
-    <div class="container-twitch">
-        <h2 class="category">Directos</h2>
-        <div id="twitch-embed">
-
-        </div>
-
+        <?php } ?>
     </div>
-</div>
-<?php include "include/final-divs.php" ?>
-<script src="https://player.twitch.tv/js/embed/v1.js"></script>
 
-<script type="text/javascript">
-    $(document).ready(function() {
-        $(".slider").slick({
-            lazyLoad: 'ondemand',
-            autoplay: true,
-            autoplaySpeed: 1200,
-            infinite: true,
-        });
-        $(".container-juego").slick({
-            dots: false,
-            infinite: true,
-            slidesToShow: 3,
-            slidesToScroll: 3
-        });
-        $("#twitch-embed").slick({
-            slidesToShow: 2,
-            slidesToScroll: 1
-        });
-    });
-</script>
-<script type="text/javascript">
-    new Twitch.Player("twitch-embed", {
-        channel: "rubius",
-        height: "250px",
-        width: "400px",
-        muted: "true",
-    });
-    new Twitch.Player("twitch-embed", {
-        channel: "nicolasarc",
-        height: "250px",
-        width: "400px",
-        muted: "true",
-    });
-    new Twitch.Player("twitch-embed", {
-        channel: "randomtopicgames",
-        height: "250px",
-        width: "400px",
-        muted: "true",
-    });
-</script>
+    <div class="body-homepage">
+        <?php foreach ($results['categorias'] as $categoria) { ?>
+            <div class="container-categoria">
+                <h2 class="category"> <a href="categoria_<?php echo $categoria->id ?>"><?php echo htmlspecialchars($categoria->nombre) ?></a> </h2>
+                <div class="container-juego">
+                    <?php foreach ($results['juegos'] as $juego) { ?>
+                        <?php if ($juego->categoriaId == $categoria->id) { ?>
+                            <div class="slide-juego">
 
-<?php include "include/footer.php" ?>
+                                <div class="slide-img2">
+                                    <a href="juego_<?php echo $juego->id ?>"><img src="<?php echo JUEGO_IMAGE_PATH . "/" . htmlspecialchars($juego->getImages(0)) ?>"></a>
+                                </div>
+                                <div class="slide-title-juego">
+                                    <a href="juego_<?php echo $juego->id ?>"><span> <?php echo ($juego->nombre) ?></span></a>
+                                </div>
+
+                            </div>
+                        <?php } ?>
+                    <?php } ?>
+                </div>
+                <h3><a href="categoria_<?php echo $categoria->id ?>">VER MAS</a></h3>
+            </div>
+        <?php } ?>
+
+        <div class="container-twitch">
+            <h2 class="category">Directos</h2>
+            <div id="twitch-embed">
+
+            </div>
+
+        </div>
+    </div>
+    <?php include "include/final-divs.php" ?>
+    <script src="https://player.twitch.tv/js/embed/v1.js"></script>
+
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $(".slider").slick({
+                lazyLoad: 'ondemand',
+                autoplay: true,
+                autoplaySpeed: 1200,
+                infinite: true,
+            });
+            $(".container-juego").slick({
+                dots: false,
+                infinite: true,
+                slidesToShow: 3,
+                slidesToScroll: 3
+            });
+            $("#twitch-embed").slick({
+                slidesToShow: 2,
+                slidesToScroll: 1
+            });
+        });
+    </script>
+    <script type="text/javascript">
+        new Twitch.Player("twitch-embed", {
+            channel: "rubius",
+            height: "250px",
+            width: "400px",
+            muted: "true",
+        });
+        new Twitch.Player("twitch-embed", {
+            channel: "nicolasarc",
+            height: "250px",
+            width: "400px",
+            muted: "true",
+        });
+        new Twitch.Player("twitch-embed", {
+            channel: "randomtopicgames",
+            height: "250px",
+            width: "400px",
+            muted: "true",
+        });
+    </script>
+
+    <?php include "include/footer.php" ?>
--- templates/include/header.php
+++ templates/include/header.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html lang="es">
-<?php use  css\estilos; ?>
+<?php
+
+use  css\estilos; ?>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,7 +15,6 @@
 </head>
 
 <body>
-
     <div class="container-menu">
         <div class="menu">
             <div class="logo">
@@ -25,9 +27,38 @@
                             <li><a href="categoria_<?php echo $categoria->id ?>"><?php echo htmlspecialchars($categoria->nombre) ?></a></li>
 
                         <?php } ?>
+                        <?php if( $results['categorias']){ ?>
+                        <li>
+                            <div class="icon-menu1 ctn-icon-search">
+                                <img id="icon-search" src="img/busqueda.png" alt="">
+                            </div>
+                        </li>
+                        <?php } ?>
                     </ul>
                 </nav>
             </div>
         </div>
     </div>
+    <div id="ctn-bars-search">
+        <input type="text" id="inputSearch" placeholder="¿Qué deseas buscar?">
+    </div>
+    <ul id="box-search">
+        <?php foreach ($results['juegos'] as $juegolista) { ?>
+            <li>
+                <a href="juego_<?php echo $juegolista->id ?>">
+                    <i class="fas fa-search">
+                    </i>
+                    <?php echo htmlspecialchars($juegolista->nombre) ?>
+                </a>
+            </li>
+        <?php } ?>
+    </ul>
+    <div id="ctn-icon-search">
+        <i class="fas fa-search" id="icon-search"></i>
+    </div>
+    <div id="cover-ctn-search"></div>
+    <div id="icon-menu">
+        <i class="fas fa-bars"></i>
+    </div>
+
     <div class="container">
