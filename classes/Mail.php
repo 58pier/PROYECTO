b70f953c1319da9898e245415a26d265239666e4
Piero <pieropozo58@gmail.com>
2020-08-28 11:02:35 -0500

    Update Mail.php

--- classes/Mail.php
+++ classes/Mail.php
@@ -157,22 +157,19 @@ tr {
 
         try {
             //Server settings
-            $mail->SMTPDebug = 0;                      // Enable verbose debug output
-            $mail->isSMTP();                                            // Send using SMTP
-            $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
-            $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-            $mail->Username   = 'pieropozo58@gmail.com';                     // SMTP username
-            $mail->Password   = "EPm}&iF6H/''";                               // SMTP password
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-            $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+            $mail->SMTPDebug = 0;
+            $mail->isSMTP();
+            $mail->Host       = 'smtp.gmail.com'; 
+            $mail->SMTPAuth   = true;
+            $mail->Username   = 'pieropozo58@gmail.com';
+            $mail->Password   = "asd";
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+            $mail->Port       = 587;
 
-            //Recipients
             $mail->setFrom('pieropozo58@gmail.com');
-            $mail->addAddress($this->correo);     // Add a recipient
+            $mail->addAddress($this->correo);
 
-
-            // Content
-            $mail->isHTML(true);                                  // Set email format to HTML
+            $mail->isHTML(true);
             $mail->Subject = 'Prueba correo';
             $mail->Body    = $this->mensaje;
 
